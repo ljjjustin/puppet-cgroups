2348f80ebf274502749ba9f95dce40d582418454
Jiajun Liu <jiajun@unitedstack.com>
2014-04-04 15:54:56 +0800

    add class cgroups::rule

---
+++ manifests/rule.pp
@@ -0,0 +1,43 @@
+# == Class: cgroups::rule
+#
+# This class define a cgroup rule.
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+#  cgroups::rule { 'root':
+#    user        => 'root',
+#    controllers => '*',
+#    destination => 'root/',
+#  }
+#
+define cgroups::rule (
+  $user = false,
+  $process = false,
+  $controllers = false,
+  $destination = false,
+) {
+
+  # check parameter
+  if ! $user or ! $controllers or ! $destination {
+    fail("You MUST provide 'user', 'controllers' and 'destination' in order to define a rule")
+  }
+
+  if $process {
+    $real_user = "${user}:${process}"
+  } else {
+    $real_user = $user
+  }
+  
+  # modify cgroup rules
+  file_line { $real_user:
+    path    => $::cgroups::cgrules_conf,
+    match   => "$real_user ",
+    line    => "$real_user $controllers $destination",
+    require => Package[$::cgroups::package_name],
+    notify  => Service[$::cgroups::cgred_service_name],
+  }
+}
